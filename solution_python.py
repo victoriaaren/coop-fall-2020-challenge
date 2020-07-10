ec1bdb0ca1b13c8a7a1863ec1907a779d62558b5
Victoria Ren <vjren@uwaterloo.ca>
2020-07-10 14:14:06 -0400

    first commit

--- solution_python.py
+++ solution_python.py
@@ -1,23 +1,42 @@
 class EventSourcer():
     # Do not change the signature of any functions
-
+    
     def __init__(self):
         self.value = 0
+        self.undoStack = []
+        self.redoStack = []
 
     def add(self, num: int):
-        pass
+        self.value+=int
+        self.undoStack.append(self.value)
 
     def subtract(self, num: int):
-        pass
+        self.value-=int
+        self.undoStack.append(self.value)
 
     def undo(self):
-        pass
+      if(len(self.undoStack)>0):
+        self.redoStack.append(self.undoStack.pop())
+        self.value = self.undoStack[len(self.undoStack) -1]
+      return self.value
+
 
     def redo(self):
-        pass
+        if(len(self.redoStack) >0):
+          self.undoStack.append(self.redoStack.pop())
+          self.value = self.undoStack[len(self.undoStack) -1]
+        return self.value
 
     def bulk_undo(self, steps: int):
-        pass
+        if(len(self.undoStack)>=steps):
+          for i in range(steps):
+            self.redoStack.append(self.undoStack.pop())
+          self.value = self.undoStack[len(self.undoStack)-1]
+        return self.value
 
     def bulk_redo(self, steps: int):
-        pass
+      if(len(self.redoStack)>=steps):
+        for i in range(steps):
+          self.undoStack.append(self.redoStack.pop())
+        self.value = self.undoStack[len(self.undoStack)-1]
+      return self.value
